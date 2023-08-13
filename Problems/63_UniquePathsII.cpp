f080578c3e75ff8888473c9f3a05d5f8da1d749e
William Clark <Austin.farback9@gmail.com>
2023-08-12 18:04:47 -0700

    [Main Branch]  - Completed Question 63

---
+++ Problems/63_UniquePathsII.cpp
@@ -0,0 +1,139 @@
+//#include <iostream>
+//#include <vector>
+//#include <array>
+//
+//// Question
+///*
+//	63. Unique Paths II - August 25th, 2023
+//	You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+//
+//	An obstacle and space are marked as 1 or 0 respectively in grid. A path that the robot takes cannot include any square that is an obstacle.
+//
+//	Return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+//
+//	The testcases are generated so that the answer will be less than or equal to 2 * 10^9.
+//*/
+//
+//// Examples
+///*
+//	Example 1:
+//	Input: obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+//	Output: 2
+//	Explanation: There is one obstacle in the middle of the 3x3 grid above.
+//	There are two ways to reach the bottom-right corner:
+//	1. Right -> Right -> Down -> Down
+//	2. Down -> Down -> Right -> Right
+//
+//	Example 2:
+//	Input: obstacleGrid = [[0,1],[0,0]]
+//	Output: 1
+//*/
+//
+//// Constraints
+///*
+//	m == obstacleGrid.length
+//	n == obstacleGrid[i].length
+//	1 <= m, n <= 100
+//	obstacleGrid[i][j] is 0 or 1.
+//*/
+//
+//namespace Question_63
+//{
+//	int UniquePathsWithObstacles(std::vector<std::vector<int>>& obstacleGrid)
+//	{
+//		int n = static_cast<int>(obstacleGrid.size());
+//		int m = static_cast<int>(obstacleGrid[0].size());
+//		std::vector<std::vector<int>> ans(n, std::vector<int>(m, 0));
+//
+//		// Fill top row
+//		for (int i = 0; i < n; ++i)
+//		{
+//			if (obstacleGrid[i][0] == 0)
+//			{
+//				ans[i][0] = 1;
+//			}
+//			else
+//			{
+//				break;
+//			}
+//		}
+//
+//		// Fill left column
+//		for (int j = 0; j < m; ++j) {
+//			if (obstacleGrid[0][j] == 0)
+//			{
+//				ans[0][j] = 1;
+//			}
+//			else
+//			{
+//				break;
+//			}
+//		}
+//
+//		// Fill rows and columns
+//		for (int i = 1; i < n; ++i)
+//		{
+//			for (int j = 1; j < m; ++j)
+//			{
+//				if (obstacleGrid[i][j] == 0)
+//				{
+//					ans[i][j] = ans[i - 1][j] + ans[i][j - 1];
+//				}
+//			}
+//		}
+//
+//		return ans[n - 1][m - 1];
+//	}
+//}
+//
+//using namespace Question_63;
+//// Test the code using the examples
+//int main()
+//{
+//	// Example 1 = 2
+//	std::vector<std::vector<int>> list1 = { {0, 0, 0}, {0, 1, 0}, {0, 0, 0} };
+//	std::cout << UniquePathsWithObstacles(list1) << std::endl;
+//
+//	// Example 2 = 1
+//	std::vector<std::vector<int>> list2 = { {0, 1}, {0, 0} };
+//	std::cout << UniquePathsWithObstacles(list2) << std::endl;
+//
+//	// Example 3 = 1
+//	std::vector<std::vector<int>> list3 = { {0, 0, 0}, {0, 1, 0}, {0, 1, 1}, {0, 0, 0} };
+//	std::cout << UniquePathsWithObstacles(list3) << std::endl;
+//
+//	// Example 4 = 1
+//	std::vector<std::vector<int>> list4 = { {0} };
+//	std::cout << UniquePathsWithObstacles(list4) << std::endl;
+//
+//	// Example 5
+//	std::vector<std::vector<int>> list5 = { {0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0},
+//		{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//		{1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0},
+//		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1},
+//		{0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+//		{0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0},
+//		{0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0},
+//		{1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1},
+//		{0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+//		{0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+//		{1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0},
+//		{0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1},
+//		{0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0},
+//		{1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//		{0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+//		{0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+//		{0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+//		{0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+//		{0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//		{0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0},
+//		{0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+//		{0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+//		{0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//		{1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+//		{0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0},
+//		{0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0},
+//		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0} };
+//	std::cout << UniquePathsWithObstacles(list5) << std::endl;
+//}
